e51d633d43c67638fcfa045ceccd581c8cd589c8
marc8s <marcus.ecomp@gmail.com>
2016-04-22 23:21:43 -0300

    Aprimorando o layout

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ include_once("connect.php");
 		                <div class="collapse navbar-collapse navbar-right">
 		                    <ul class="nav navbar-nav">
 		                        <li class="scroll active"><a href="#main-slider">Home</a></li>
-		                        <li class="scroll"><a href="#sessao2">Como Funciona</a></li>
+		                        <li class="scroll"><a href="#about">Como Funciona</a></li>
 		                        <li class="scroll"><a href="#sessao3">Login</a></li>
 		                        <li class="scroll"><a href="#sessao4">Mapa</a></li>                 
 		                                               
@@ -201,10 +201,50 @@ include_once("connect.php");
         </div><!--/.owl-carousel-->			
 			</section>	
 					
-			<section id="sessao2" class="size-section row content-justify">
-				<br><br><br><br>
-				<header></header>
-				<footer></footer>
+			<section id="about" class=" ">
+				<div class="container">
+
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">Como Funciona</h2>
+                <p class="text-center wow fadeInDown">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6 wow fadeInLeft">
+                    <h3 class="column-title">Tutorial</h3>
+                    <!-- 16:9 aspect ratio -->
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="//player.vimeo.com/video/58093852?title=0&amp;byline=0&amp;portrait=0&amp;color=e79b39" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 wow fadeInRight">
+                    <h3 class="column-title">Multi Capability</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <ul class="nostyle">
+                                <li><i class="fa fa-check-square"></i> Ipsum is simply dummy</li>
+                                <li><i class="fa fa-check-square"></i> When an unknown</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <ul class="nostyle">
+                                <li><i class="fa fa-check-square"></i> The printing and typesetting</li>
+                                <li><i class="fa fa-check-square"></i> Lorem Ipsum has been</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <a class="btn btn-primary" href="#">Learn More</a>
+
+                </div>
+            </div>
+        </div>
 			</section>
 
 			<section id="sessao3" class="size-section row content-justify" style=" background: #f5f5f5">
